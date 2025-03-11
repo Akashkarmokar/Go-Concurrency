e047d9dba5b81c1ba0d8f7e02bccb07136a88b63
akash <akashcsemu@gmail.com>
2025-03-12 00:51:51 +0600

    Generator Pattern Code Added

--- Generator_Pattern/main.go
+++ Generator_Pattern/main.go
@@ -1,28 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-func generator(maxNumber int, intChan chan<- int, parentWg *sync.WaitGroup) {
-	defer parentWg.Done()
-	for i := 1; i <= maxNumber; i++ {
-		intChan <- i
-	}
-	close(intChan)
+func generateNumber(num int) chan int {
+
+	// Create a chanel
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+
+		for i := 1; i <= num; i++ {
+			out <- i
+		}
+	}()
+
+	// return chanel
+	return out
 }
 
 func main() {
-	intChan := make(chan int)
-	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go generator(5, intChan, &wg)
+	ourChanel := generateNumber(4)
 
-	for value := range intChan {
-		fmt.Println("Values: ", value)
+	// recieve
+
+	for num := range ourChanel {
+		fmt.Println("Reviews values: ", num)
 	}
-	wg.Wait()
-
 }
